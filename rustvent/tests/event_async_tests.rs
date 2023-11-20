27f9d69940bc6d3539251df03022aec5de0157f8
Tim Bush <tb674d@gmail.com>
2023-11-20 08:08:55 -0500

    Implement SubscriberAsyncMut

--- rustvent/tests/event_async_tests.rs
+++ rustvent/tests/event_async_tests.rs
@@ -1,6 +1,8 @@
 #[cfg(test)]
 mod event_async_tests {
-    use rustvent::{event_async::EventAsync, subscriber::SubscriberAsync};
+    use std::sync::{Arc, Mutex};
+
+    use rustvent::{event_async::EventAsync, subscriber::{SubscriberAsync, SubscriberAsyncMut}};
 
     struct ProcessBusinessLogic {
         process_completed: EventAsync
@@ -59,6 +61,64 @@ mod event_async_tests {
         logic.on_process_completed();  
 
         assert_eq!(1, logic.process_completed.times_subscribers_notified); 
+    }
+
+    #[test]
+    fn event_async_subscribers_can_call_method_on_self_when_notified() {
+        let mut logic = ProcessBusinessLogic {
+            process_completed: EventAsync::default()
+        };
+
+        struct SomeSubscriber {}
+
+        impl SomeSubscriber {
+            fn do_work(&self) {
+                println!("SomeSubscriber doing work...");
+            }
+        }
+        impl SubscriberAsync for SomeSubscriber {
+            fn update(&self) {
+                self.do_work();
+            }
+        }
+
+        let some_sub = SomeSubscriber {}.into_arc();
+        logic.process_completed.subscribe(some_sub);               
+        logic.on_process_completed();  
+
+        assert_eq!(1, logic.process_completed.times_subscribers_notified); 
+    }
+
+    #[test]
+    fn event_async_subscribers_can_mutate_methods_on_self_when_notified() {
+        pub struct Logic {
+            process_completed: EventAsync
+        }
+        impl Logic {
+            fn on_process_completed(&mut self) {
+                self.process_completed.notify_subscribers_mut();
+            }
+        }
+
+        struct SomeSubscriber {
+            pub field_to_mutate: u8
+        }
+
+        impl SubscriberAsyncMut for SomeSubscriber {
+            fn update_mut(&mut self) {
+                self.field_to_mutate += 10
+            }
+        }
+
+        let mut logic = Logic {
+            process_completed: EventAsync::default()
+        };
+
+        let some_sub = Arc::new(Mutex::new(SomeSubscriber { field_to_mutate: 10 }));
+        logic.process_completed.subscribe_mut(some_sub.clone());               
+        logic.on_process_completed();  
+    
+        assert_eq!(20, some_sub.clone().lock().unwrap().field_to_mutate); 
     }
 
     #[test]
